7a94d76dae0578def2fdb85aa8ccab9215be4e4e
Adam Spofford <pieflavor.mc@gmail.com>
2020-05-10 04:59:17 -0700

    Remove client-side stuff, to be continued on branch clientside

--- src/main/java/flavor/pie/pieconomy/client/PieconomyPacketHandler.java
+++
@@ -1,8 +0,0 @@
-package flavor.pie.pieconomy.client;
-
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-
-public class PieconomyPacketHandler {
-    public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("pieconomy");
-}
